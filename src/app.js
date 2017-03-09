affc0b0168f07a7dc718aa26375e85742d1a12f9
Lykov Artem <a.lykov921@gmail.com>
2017-03-09 23:19:25 +0300

    auth added

--- src/app.js
+++ src/app.js
@@ -7,53 +7,62 @@ angular.module('myApp', [
     'ngAnimate',
     'ui.bootstrap'
 ])
-    .factory('sharedService', ['$filter', function ($filter) {
-        let sharedService = {
-            showTasksByState: function (selectedState = 'all') {
+    .factory('sharedService', ['$filter',
+        function ($filter) {
+            let sharedService = {
+                showTasksByState: function (selectedState = 'all') {
 
-                let tasksByState = $filter('filter')(sharedService.todos,
-                    item => {
-                        return selectedState == 'all' ? item : item.taskState == selectedState
+                    let tasksByState = $filter('filter')(sharedService.todos,
+                        item => {
+                            return selectedState == 'all' ? item : item.taskState == selectedState
+                        });
+
+                    let selectedTitle = sharedService.states.find(item => {
+                        return item.state == selectedState
                     });
 
-                let selectedTitle = sharedService.states.find(item => {
-                    return item.state == selectedState
-                });
+                    sharedService.tasksToShow = {
+                        header: selectedTitle ? selectedTitle.title : "All",
+                        data: tasksByState
+                    };
 
-                sharedService.tasksToShow = {
-                    header: selectedTitle ? selectedTitle.title : "All",
-                    data: tasksByState
-                };
-
-                sharedService.selectedState = selectedState;
-            },
-            isLoggedIn: false
-        };
-        return sharedService;
-    }])
-    .factory('dataService', ['$http', '$q', function ($http, $q) {
-        return {
-            getTasksAndStates: function () {
-                return $q.all([
-                    $http.get('./api/all-tasks'),
-                    $http.get('./data/states.json')
-                ]);
-            },
-            addNewTask: function (task) {
-                return $http.post('/api/create-task', task);
-            },
-            editTask: function (task) {
-                return $http.put(`/api/update-task/${task._id}`, task);
-            },
-            deleteTask: function (task) {
-                return $http.delete(`/api/delete-task/${task._id}`, task);
+                    sharedService.selectedState = selectedState;
+                },
+                isLoggedIn: false
+            };
+            return sharedService;
+        }])
+    .factory('dataService', ['$http', '$q', 'sharedService',
+        function ($http, $q, sharedService) {
+            return {
+                getTasksAndStates: function () {
+                    return $q.all([
+                        $http.get(`./api/all-tasks`),
+                        $http.get('./data/states.json')
+                    ]);
+                },
+                addNewTask: function (task) {
+                    return $http.post('/api/create-task', task);
+                },
+                editTask: function (task) {
+                    return $http.put(`/api/update-task/${task._id}`, task);
+                },
+                deleteTask: function (task) {
+                    return $http.delete(`/api/delete-task/${task._id}`, task);
+                }
             }
-        }
-    }])
-    .factory('AuthenticationService', ['$http',
+        }])
+    /*
+    * {
+     task,
+     user: sharedService.currentUser
+     }
+    * */
+
+    .factory('authenticationService', ['$http',
         function ($http) {
             return {
-                login: function (username, password, callback) {
+                logIn: function (username, password, callback) {
                     $http.post('/api/log-in', {username, password})
                         .then(res => {
                             if (callback) {
@@ -64,9 +73,7 @@ angular.module('myApp', [
                 register: function (username, password, callback) {
                     $http.post('/api/register', {username, password})
                         .then(res => {
-                            if (callback) {
-                                callback(res)
-                            }
+                            if (callback) callback(res)
                         })
                 }
             }
