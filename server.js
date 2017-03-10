2ff6796612a387765af7beff38c76f915ea3eb76
Artsiom Lykau <Artsiom_Lykau@epam.com>
2017-03-10 16:09:31 +0300

    some features

--- server.js
+++ server.js
@@ -4,12 +4,17 @@
 
 // set up ========================
 let express = require('express');
+let session = require('express-session');
+let Session = session.Session;
+let cookieParser = require('cookie-parser');
 let app = express();
 let mongoose = require('mongoose');
 let Schema = mongoose.Schema;
 let morgan = require('morgan');                     // log requests to the console (express4)
 let bodyParser = require('body-parser');            // pull information from HTML POST (express4)
 let methodOverride = require('method-override');    // simulate DELETE and PUT (express4)
+let connect = require('connect');
+let MongoStore = require('connect-mongo')(session);
 
 let bcrypt = require('bcrypt');
 const SALT_WORK_FACTOR = 10;
@@ -17,10 +22,16 @@ const SALT_WORK_FACTOR = 10;
 // configuration =================
 const PORT = 8080;
 
-mongoose.connect('mongodb://admin:admin@ds163699.mlab.com:63699/wunderlist');     // connect to mongoDB database
+mongoose.connect('mongodb://admin:admin@ds163699.mlab.com:63699/wunderlist');     // connect to mongoDB
 
 app.use(express.static(__dirname + '/'));
 app.use(morgan('dev'));                                         // log every request to the console
+app.use(cookieParser());
+app.use(session({
+    secret: 'keyboard cat',
+    // key: 'keyboard cat',
+    store: new MongoStore({mongooseConnection: mongoose.connection})
+}));
 app.use(bodyParser.urlencoded({'extended': 'true'}));           // parse application/x-www-form-urlencoded
 app.use(bodyParser.json());                                     // parse application/json
 app.use(methodOverride());
@@ -83,18 +94,18 @@ UserSchema.methods.comparePassword = function (candidatePassword, cb) {
 let UserModel = mongoose.model('UserModel', UserSchema);
 
 // routes ========================
-app.post('/api/log-in', function (req, res) {
+app.post('/api/log-in', function (req, res, next) {
     let username = req.body.username;
     let password = req.body.password;
 
     UserModel.findOne({username}, function (err, user) {
         if (err) throw err;
-
         user.comparePassword(password, function (err, isMatch) {
             if (err) throw err;
             if (isMatch) {
-                res.send(user._id);
-                app.currentUser = user._id;
+                req.session.number = req.session.number + 1 || 1;
+                req.session.currentUser = user._id;
+                res.send(req.session);
             }
         });
     });
@@ -113,7 +124,8 @@ app.post('/api/register', function (req, res) {
 });
 
 app.get('/api/all-tasks', function (req, res) {
-    UserModel.findOne({_id: app.currentUser}, function (err, user) {
+    console.log(req.session.currentUser);
+    UserModel.findOne({_id: req.session.currentUser}, function (err, user) {
         if (err) return handleError(err);
         res.json(user.tasks);
     });
@@ -121,25 +133,45 @@ app.get('/api/all-tasks', function (req, res) {
 
 app.post('/api/create-task', function (req, res) {
     let data = req.body;
-    let newTask = new TaskModel(data);
-    UserModel
-        .findOne({_id: app.currentUser})
-        .tasks.push(data);
-    newTask.save(function (err) {
+    UserModel.update({_id: req.session.currentUser}, {
+        $push: {'tasks': data}
+    }, function (err) {
         if (err) return handleError(err);
-    });
+    })
 });
 
 app.put('/api/update-task/:_id', function (req, res) {
     let data = req.body;
-    TaskModel.update({_id: req.params._id}, {
-        name: data.name,
-        description: data.description,
-        hours: data.hours,
-        taskState: data.taskState
+    let _id = req.params._id;
+    console.log(data);
+    /*    TaskModel.update({_id: req.params._id}, {
+     name: data.name,
+     description: data.description,
+     hours: data.hours,
+     taskState: data.taskState
+     }, function (err) {
+     if (err) return handleError(err);
+     })*/
+
+    /*    UserModel.findOne({_id: req.session.currentUser},
+     function (err, user) {
+     if (err) return handleError(err);
+     let task = user.tasks.find((it, i, arr) => {
+     return it._id == _id
+     });
+     task = data;
+     });*/
+    UserModel.update({_id: req.session.currentUser, 'tasks._id': _id}, {
+        $set: {
+            'tasks.$.name': data.name,
+            'tasks.$.description': data.description,
+            'tasks.$.hours': data.hours,
+            'tasks.$.taskState': data.taskState,
+        },
     }, function (err) {
         if (err) return handleError(err);
     })
+
 });
 
 app.delete('/api/delete-task/:_id', function (req, res) {
